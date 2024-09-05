e380cd56311676b529d4eff6d4d5edd01cf672e8
sanjeetgit1 <sksanjeet616@gmail.com>
2024-09-05 16:00:35 +0530

    final commit

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,17 +1,24 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './Main.css'
 import { assets } from '../../assets/assets'
+import { Context } from '../../context/Context'
 
 const Main = () => {
+
+    const {onSent,recentPrompt, showResult, loading, resultData, setInput,input }=useContext(Context)
+
+
   return (
     <div className='main'>
         <div className='nav'>
             <p>Gemini</p>
             <img src={assets.user_icon} alt="" />
 
+
         </div>
         <div className="main-container">
-            <div className="greet">
+
+            {!showResult?<> <div className="greet">
                 <p><span>Hello, champ...</span></p>
                 <p>How can I help you Today?</p>
             </div>
@@ -32,15 +39,37 @@ const Main = () => {
                     <p>Suggest beautiful places to see on an upcoming road trip</p>
                     <img src={assets.code_icon} alt="" />
                 </div>
+            </div></>
+            : <div className='result'>
+                <div className='result-title'>
+                    <img src={assets.user_icon} alt="" />
+                    <p>{recentPrompt}</p>
+
+                </div>
+                <div className='result-data'>
+                    <img src={assets.gemini_icon} alt="" />
+                    {loading
+                    ?<div className='loader'>
+                        <hr />
+                        <hr />
+                        <hr />
+                    </div>:<p dangerouslySetInnerHTML={{__html:resultData}}></p>}
+                    
+
+                </div>
+
             </div>
+            }
+           
 
             <div className="main-bottom">
                 <div className='search-box'>
-                    <input type="text" placeholder='Enter a Prompt here' />
+                    <input onChange={(e)=>setInput(e.target.value)}  value={input} type="text" placeholder='Enter a Prompt here' />
                     <div>
                         <img src={assets.gallery_icon} alt="" />
                         <img src={assets.mic_icon} alt="" />
-                        <img src={assets.send_icon} alt="" />
+
+                      {input ?<img onClick={()=>onSent()} src={assets.send_icon} alt="" />:null}  
                     </div>
 
                 </div>
--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -1,39 +1,77 @@
 import { createContext, useState } from "react";
 import run from "../config/gemini";
 
-export const Context =createContext();
-const ContextProvider=(props)=>{
+export const Context = createContext();
+const ContextProvider = (props) => {
+  const [input, setInput] = useState("");
+  const [recentPrompt, setRecentPrompt] = useState("");
+  const [prevPrompt, setPrevPrompt] = useState([]);
+  const [showResult, setShowResult] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [resultData, setResultData] = useState("");
 
-    const[input,setInput]= useState("");
-    const[recentPrompt,setRecentPrompt]=useState("");
-    const[prevPrompt,setPrevPrompt]=useState([]);
-    const[showResult,setShowResult]=useState(false);
-    const[loading,setLoading]=useState(false);
-    const[resultData,setResultData]=useState("");
+  const delayPara = (index, nextword) => {
+    setTimeout(function () {
+      setResultData((prev) => prev + nextword);
+    }, 75 * index);
+  };
 
+  const newChat =()=>{
+    setLoading(false);
+    setShowResult(false)
+  }
 
+  const onSent = async (prompt) => {
+    setResultData("");
+    setLoading(true);
+    setShowResult(true);
+    let result; 
+    if(prompt !== undefined){
+        result= await run(prompt);
+        setRecentPrompt(prompt)
 
+    }else{
+        setPrevPrompt(prev=>[ ...prev, input])
+        setRecentPrompt(input)
+        result=await run(input)
 
-    const onSent= async(prompt)=>{
-    await run(prompt)
     }
    
-    const contextValue={
-        prevPrompt,
-        setPrevPrompt,
-        onSent,
-        setRecentPrompt,recentPrompt,
-        showResult,
-        loading,
-        resultData,input,setInput,
-        
-
+    let responseArray = result.split("**");
+    let newResult = "";
+    for (let i = 0; i < responseArray.length; i++) {
+      if (i === 0 || i % 2 !== 1) {
+        newResult += responseArray[i];
+      } else {
+        newResult += "<b>" + responseArray[i] + "</b>";
+      }
     }
-    return(
-        <Context.Provider value={contextValue}>
-            {props.children}
-        </Context.Provider>
-    )
-}
+    let newResult2 = newResult.split("*").join("</br>");
+    let newResultArray = newResult2.split(" ");
+    for (let i = 0; i < newResultArray.length; i++) {
+      const nextword = newResultArray[i];
+      delayPara(i, nextword + " ");
+    }
+    setLoading(false);
+    setInput("");
+  };
 
-export default ContextProvider
+  const contextValue = {
+    prevPrompt,
+    setPrevPrompt,
+    onSent,
+    setRecentPrompt,
+    recentPrompt,
+    showResult,
+    loading,
+    resultData,
+    input,
+    setInput,
+    newChat
+  };
+  return (
+    <Context.Provider value={contextValue}>{props.children}</Context.Provider>
+  );
+};
+
+export default ContextProvider;
