fd204be2475662273cc3d04b77105d851068f8f7
sanjeetgit1 <sksanjeet616@gmail.com>
2024-09-06 16:35:29 +0530

    2.0

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -11,7 +11,7 @@ const Main = () => {
   return (
     <div className='main'>
         <div className='nav'>
-            <p>Gemini</p>
+            <p>Gemini : 2.0</p>
             <img src={assets.user_icon} alt="" />
 
 
